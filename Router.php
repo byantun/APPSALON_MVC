b0cd57cc323be20c6ad7ea31eed0344e92f9be6f
byantun <diegorolandoa@gmail.com>
2022-06-28 22:22:28 -0400

    Deployment de Router.php

--- Router.php
+++ Router.php
@@ -29,7 +29,7 @@ class Router
         // $auth = $_SESSION['login'] ?? null;
 
         $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
-        $method = $_SERVER['REQUEST_METHOD'];
+        $method = $_SERVER['REQUEST_URI'];
 
         if ($method === 'GET') {
             $fn = $this->getRoutes[$currentUrl] ?? null;
